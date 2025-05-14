7b125ee9496ea77b1a68fb3e80eb3f8df3a39773
Armen <armenhaddade@gmail.com>
2025-05-14 16:16:57 +0200

    fixed scoring functions

--- main/test.py
+++ main/test.py
@@ -5,8 +5,5 @@ import time
 start = time.time()
 
 df = parse_file("data/10_computable_moments.txt")
-#get_tags_with_paintings(df, "data/output/tags_paintings_frq.tags")
-#df_analysis(df, plot=True)
-print(global_score("data/output/output_yuke.txt", df))
-end = time.time()
+
 print(f"Execution time: {end - start:.4f} seconds")
