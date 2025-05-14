daa4baf35c2599008ff1b3fb82ecf3d0246e42d9
Armen <armenhaddade@gmail.com>
2025-05-14 13:52:40 +0200

    minor adj

--- main/test.py
+++ main/test.py
@@ -6,6 +6,6 @@ start = time.time()
 
 df = parse_file("data/110_oily_portraits.txt")
 #write_random_order(df, "data/output/oily.txt")
-
+write_tags_with_paintings(df, "data/output/tags_paintings_frq.tags")
 end = time.time()
 print(f"Execution time: {end - start:.4f} seconds")
